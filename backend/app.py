ccb61e801a97bec68097fb9d4bb26495a349dc7f
LucaCilibrasi <37807618+LucaCilibrasi@users.noreply.github.com>
2021-07-29 17:25:33 +0200

    - All graphical changes

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from flask_executor import Executor
 
 from apis import api_blueprint
 
-base_url = '/viruclust/'
+base_url = '/viruclust_gisaid/'
 api_url = base_url + 'api'
 repo_static_url = base_url + 'repo_static'
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -13,5 +13,5 @@ module.exports = {
           },
       }
   },
-  baseUrl: '/viruclust/',
+  baseUrl: '/viruclust_gisaid/',
 }
